1f703f195ce60142adbdd36f3de83ef5a45cd2d6
joao <joaobpedro@gmail.com>
2026-02-28 16:54:00 +0100

    change my wezterm config a bit

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,7 @@ local config = {}
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
+config.window_background_opacity = 0.9
 
 -- windows specific config
 -- Detect if running on Windows
@@ -32,7 +33,7 @@ local tab_style = "square"
 
 config.font =
     wezterm.font_with_fallback { "BerkeleyMono Nerd Font", "BerkeleyMono Nerd Font" }
-config.font_size = 14
+config.font_size = 13
 
 config.window_decorations = "RESIZE"
 
@@ -124,9 +125,90 @@ for i = 1, 9 do
 end
 
 -- tab bar
-config.hide_tab_bar_if_only_one_tab = false
+config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = false
 
+config.colors = {
+  background = '#000000',
+  foreground = '#C1C497',
+  
+  -- Cursor colors
+  cursor_bg = '#D7C995',
+  cursor_fg = '#000000',
+  cursor_border = '#D7C995',
+
+  -- Selection colors (when you highlight text)
+  selection_bg = '#C1C497',
+  selection_fg = '#111C18',
+  
+  -- The dark accents for the terminal blacks/greys
+  ansi = {
+    '#23372B', -- Black (Color 0)
+    '#cc0000', -- Red (Replace with your preferred red)
+    '#4e9a06', -- Green (Replace with your preferred green)
+    '#c4a000', -- Yellow (Replace with your preferred yellow)
+    '#3465a4', -- Blue (Replace with your preferred blue)
+    '#75507b', -- Magenta (Replace with your preferred magenta)
+    '#06989a', -- Cyan (Replace with your preferred cyan)
+    '#d3d7cf', -- White (Replace with your preferred white)
+  },
+  brights = {
+    '#53685B', -- Bright Black / Grey (Color 8)
+    '#ef2929', -- Bright Red
+    '#8ae234', -- Bright Green
+    '#fce94f', -- Bright Yellow
+    '#729fcf', -- Bright Blue
+    '#ad7fa8', -- Bright Magenta
+    '#34e2e2', -- Bright Cyan
+    '#eeeeec', -- Bright White
+  },
+  tab_bar = {
+    -- The background color of the tab bar itself (Deep Jade)
+    background = '#000000',
+
+    -- The active tab (Dark Forest Green background, Golden Sand text)
+    active_tab = {
+      bg_color = '#23372B',
+      fg_color = '#D7C995',
+    },
+
+    -- Inactive tabs (Deep Jade background, Muted Sage Green text)
+    inactive_tab = {
+      bg_color = '#000000',
+      fg_color = '#53685B',
+    },
+
+    -- When you hover over an inactive tab with your mouse
+    inactive_tab_hover = {
+      bg_color = '#53685B',
+      fg_color = '#111C18',
+    },
+
+    -- The "+" button to create a new tab
+    new_tab = {
+      bg_color = '#111C18',
+      fg_color = '#C1C497',
+    },
+
+    -- When you hover over the new tab button
+    new_tab_hover = {
+      bg_color = '#53685B',
+      fg_color = '#111C18',
+    },
+}
+}
+
+-- config.window_padding = {
+--   left = 0,
+--   right = 0,
+--   top = 0,
+--   bottom = 0, -- Forces no extra padding above the bottom tab bar
+-- }
+
+config.use_resize_increments =  true
+
+
 return config
+
